622f619d2b4914026d7f92ec031a66937a6a3756
josceciliano <74580957+josceciliano@users.noreply.github.com>
2020-12-10 15:35:56 -0600

    Update and rename Clientes.cs to Contactos.cs

---
+++ Entidades/Contactos.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Contactos
+    {
+        public string Nombre { get; set; }
+        public int Telefono { get; set; }
+        public string PalabraClave{ get; set; }
+        public string Correo { get; set; }
+        public string TipoContacto { get; set; }
+        public string Servicios { get; set; }
+        public Boolean EstadoCliente { get; set; }
+        
+
+        public Prestamos Prestamos = new Prestamos();
+
+        public Contactos()
+        {
+            Nombre = string.Empty;
+            Telefono = 0;
+            PalabraClave = string.Empty;
+            Correo = string.Empty;
+            TipoContacto = string.Empty;
+            Servicios = string.Empty;
+            EstadoCliente = false;
+            
+        }
+    }
+}
